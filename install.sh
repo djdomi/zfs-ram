2b599f78383747e5d2230960abc6dfe914cfd9c0
DoMeLe <1812020+djdomi@users.noreply.github.com>
2019-08-20 12:03:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp etc/systemd/system/zfs-ram.serice /etc/systemd/system/zfs-ram.serice
 cp usr/local/sbin/zfs-ram /usr/local/sbin/zfs-ram
-test /usr/bin/nano && nano /usr/local/sbin/zfs-ram && systemctl enable zfs-ram && systemctl stop zfs-ram && systemcl start zfs-ram && systemctl status zfs-ram
+
+test /usr/bin/nano && nano /usr/local/sbin/zfs-ram && systemctl enable zfs-ram && systemctl stop zfs-ram && systemctl start zfs-ram && systemctl status zfs-ram
